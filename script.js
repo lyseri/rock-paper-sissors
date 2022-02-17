6f1e716fa71e2e63c97ee31b1426da11dac3b07d
lyseri <cbmer@tutanota.com>
2022-02-17 14:45:55 -0700

    Code computerPlay function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // computerPlay Function Returns rock paper or sissors, with a 1/3 chance for each.
+function computerPlay() {
+    chance = Math.random()
+    if (chance <= .33) {
+        return "rock"
+    } else if (chance > .33 && chance < .66) {
+        return "paper"
+    } else if (chance >= .66) {
+        return "sissors"
+    }
+}
 // playRound Function Takes playerSelection and computerSelection and returns result as a string.
 // caseFormater Function Formats playerSelctions case and checks for valid options.
 // game Function Repeats playRound five times and prints tallied score at the end.
+console.log(computerPlay())
