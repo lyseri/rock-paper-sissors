bb6771c040e2b7adb6257a7bd29a5bb480ced00a
lyseri <cbmer@tutanota.com>
2022-02-17 14:27:22 -0700

    Create respository and code structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock-paper-sissors
+This is a game of rock paper sissors running in the console.
+## Learning Goals:
+ - javascript basics
+ - naming conventions
+ - variable scope
+ - functions and methods
+ - insightful comments
+ - debugging tactics
+ - pseudocode
---
+++ script.js
@@ -0,0 +1,4 @@
+// computerPlay Function Returns rock paper or sissors, with a 1/3 chance for each.
+// playRound Function Takes playerSelection and computerSelection and returns result as a string.
+// caseFormater Function Formats playerSelctions case and checks for valid options.
+// game Function Repeats playRound five times and prints tallied score at the end.
