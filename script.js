4735741fbc3fa379fbdc85aebe399276b6be623a
lyseri <cbmer@tutanota.com>
2022-02-17 23:52:34 -0700

    Code function playerPlay

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ function computerPlay() {
 }
 // playRound Function Takes playerSelection and computerSelection and returns result as a string.
 // caseFormater Function Formats playerSelctions case and checks for valid options.
+function playerPlay() {
+    selection = prompt()
+    selection = selection.toLowerCase()
+    if (selection == "rock") {
+        playerSelection = "rock"
+    } else if (selection == "paper") {
+        playerSelection = "paper"
+    } else if (selection == "sissors") {
+        playerSelection = "sissors"
+    } else {
+        console.log("Invalid selection")
+        playerPlay()
+    }
+}
 // game Function Repeats playRound five times and prints tallied score at the end.
-console.log(computerPlay())
+playerSelection = '';
+playerPlay()
+console.log(playerSelection)
